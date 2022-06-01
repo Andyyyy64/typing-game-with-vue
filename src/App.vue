5999da59a337e319250c5736b4c5c1bed4866ded
Your Name <takuyarossi77@gmail.com>
2022-06-01 13:43:01 +0900

    fixed #1

--- src/App.vue
+++ src/App.vue
@@ -1,101 +1,109 @@
 <script>
 export default {
-  data(){
+  data() {
     return {
-      startFlg:"",
-      current_question:"",
-      questions:[
+      startFlg: "",
+      current_question: "",
+      questions: [
         "apple",
         "banana",
         "chocolate",
         "donut",
         "espresso"
       ],
-      typeBox:"",
-      current_question_counts:0,
-      question_counts:0,
+      typeBox: "",
+      current_question_counts: 0,
+      question_counts: 0,
     }
   },
   methods: {
-    gameStart(){
+    gameStart() {
       this.startFlg = true
+      this.$nextTick(function () {
+        document.getElementById("typeForm").focus()
+      })
     }
   },
-  mounted(){
+  mounted() {
     this.current_question = this.questions[0]
     this.question_counts = this.questions.length
   },
-  watch:{
-    typeBox(e){
-      if(e == this.current_question){
-        this.questions.splice(0,1)
+  watch: {
+    typeBox(e) {
+      if (e == this.current_question) {
+        this.questions.splice(0, 1)
         this.current_question = this.questions[0]
-        this.typeBox=""
+        this.typeBox = ""
         this.current_question_counts = this.current_question_counts + 1
       }
     }
   },
   computed: {
-    styleobject(){
-        var width = 20 * this.current_question_counts + "%"
-        if(this.current_question_counts == 5){
-          var color = "#03a9f4"
-        }else{
-           color = "orange"
-        }
-        return {
-          "width":width,
-          "background-color":color
-        }
-      },
-    }
+    styleobject() {
+      var width = 20 * this.current_question_counts + "%"
+      if (this.current_question_counts == 5) {
+        var color = "#03a9f4"
+      } else {
+        color = "orange"
+      }
+      return {
+        "width": width,
+        "background-color": color
+      }
+    },
+  }
 }
 </script>
 
 
 <template>
-<div class="contaier">
+  <div class="contaier">
     <div class="title">
       <h1>Typing Game</h1>
       <div class="marker"></div>
     </div>
-    <button  v-if="!startFlg" @click="gameStart" class="startbutton mb-20">START</button>
+    <button v-if="!startFlg" @click="gameStart" class="startbutton mb-20">START</button>
     <div v-if="startFlg">
-      <div class="question">{{current_question}}</div>
+      <div class="question">{{ current_question }}</div>
       <div v-if="current_question_counts == question_counts" class="clear">Clear!</div>
       <div class="typeFormWrapper mb-20">
-        <input v-model="typeBox" type="text" class="typeForm">
+        <input id="typeForm" v-model="typeBox" type="text" class="typeForm">
       </div>
 
       <div class="gaugeWrapper">
         <div :style="styleobject" class="gauge"></div>
       </div>
-      <div>{{current_question_counts}}/{{question_counts}}</div>
+      <div>{{ current_question_counts }}/{{ question_counts }}</div>
     </div>
-</div>
+  </div>
 </template>
 
 <style>
-*{
+* {
   font-family: inherit;
   font-size: inherit;
   line-height: inherit;
 }
+
 template {
   font-size: 32px;
 }
+
 .mb-20 {
   margin-bottom: 20px;
 }
+
 .contaier {
   width: 400px;
   margin: 0 auto;
   text-align: center;
 }
+
 .title {
   position: relative;
   font-size: 48px;
 }
+
 .marker {
   width: 100%;
   height: 35%;
@@ -104,6 +112,7 @@ template {
   bottom: 5%;
   z-index: -1;
 }
+
 .startbutton {
   background-color: #333;
   color: #fff;
@@ -113,26 +122,33 @@ template {
   border-radius: 8px;
   cursor: pointer;
 }
+
 .startbutton:hover {
   opacity: 0.7;
 }
+
 .question {
   color: black;
 }
+
 .clear {
   color: #03a9f3;
 }
+
 .typeForm {
   text-align: center;
   outline: none;
   border: none;
 }
+
 .typeFormWrapper {
   border-bottom: 1px solid gray;
 }
+
 .gauge {
   height: 12px;
 }
+
 .gaugeWrapper {
   border: 1px solid;
   height: 12px;
